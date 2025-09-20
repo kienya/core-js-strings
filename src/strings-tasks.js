a723ac2de3aed1939dcb2fc3d1fefa08d7b5ec7b
Konstantin Kienya <kienyamail@gmail.com>
2025-09-20 21:46:52 +0300

    feat: add task solution findLongestWord

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -330,8 +330,11 @@ function isPalindrome(/* str */) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  return sentence
+    .split(' ')
+    .sort((a, b) => b.length - a.length)
+    .at(0);
 }
 
 /**
